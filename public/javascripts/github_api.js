fa10222d86d55d4ef7fb30ead0ef2370c81d26a6
Choi-Eunseok <ted12333@gmail.com>
2021-09-07 23:14:03 +0900

    c,g,e,d

--- public/javascripts/github_api.js
+++ public/javascripts/github_api.js
@@ -20,11 +20,11 @@ function get_readme(repo_name){
   fetch('https://raw.githubusercontent.com/Choi-Eunseok/'+repo_name+'/main/README.md',{
     method: 'GET',
     headers: {
-      'Content-Type': 'text/html'
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
   })
-  .then((res) => console.log(res))
-  .then((html) => console.log(html))
+  .then((res) => console.log(res.text))
+  .then((data) => console.log(data))
 }
 
 function edit_readme(repo_name, readme_content){
