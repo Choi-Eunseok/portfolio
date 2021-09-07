564b5e8a966c5b0734995e5986c84643fc0a87f8
Choi-Eunseok <ted12333@gmail.com>
2021-09-07 23:31:23 +0900

    c,g,e,d

--- public/javascripts/github_api.js
+++ public/javascripts/github_api.js
@@ -17,7 +17,7 @@ function create_repo(repo_name){
 }
 
 function get_readme(repo_name){
-  fetch('https://github.com/Choi-Eunseok/'+repo_name+'/blob/main/README.md',)
+  fetch('https://raw.githubusercontent.com/Choi-Eunseok/'+repo_name+'/main/README.md',)
   .then((res) => console.log(res))
 }
 
