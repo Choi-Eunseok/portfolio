4a937518625e2b0477c79df420cce108a55e7a34
Choi-Eunseok <ted12333@gmail.com>
2021-09-07 11:12:59 +0900

    edit txt test

--- app.js
+++ app.js
@@ -41,3 +41,67 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+function create_repo(repo_name){
+  var jsonDataObj = {
+    name : repo_name,
+    auto_init : true};
+  const options = {
+    url: 'https://api.github.com/user/repos',
+    body: jsonDataObj,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
+      'Authorization': 'token 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'}
+  };
+  request.post(options, function(error,response,body){});
+
+  var fWrite = fileObject.OpenTextFile("./public/list.txt", 8);
+	fWrite.write('#'+repo_name);
+	fWrite.close();
+}
+
+function get_readme_sha(repo_name){
+  var result_json;
+  const options = {
+    url: 'https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'
+    }
+  };
+  request.get(options, function(error,response,body){
+    result_json = res.json(JSON.parse(body));
+  });
+  return result_json.sha;
+}
+
+function edit_readme(repo_name, readme_content){
+  var today = new Date();
+  var jsonDataObj = {
+    sha : get_readme_sha(repo_name),
+    message : today.toLocaleString() + ' edit',
+    content : readme_content};
+  const options = {
+    url: 'https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',
+    body: jsonDataObj,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
+      'Authorization': 'token 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'}
+  };
+  request.put(options, function(error,response,body){});
+}
+
+function delete_repo(repo_name){
+  const options = {
+    url: 'https://api.github.com/repos/Choi-Eunseok/'+repo_name,
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
+      'Authorization': 'token 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'}
+  };
+  request.delete(options, function(error,response,body){});
+}
