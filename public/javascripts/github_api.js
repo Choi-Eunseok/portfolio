a96325e5b41c58e67102c070f1259b462da964ad
Choi-Eunseok <ted12333@gmail.com>
2021-09-07 18:16:50 +0900

    test

--- public/javascripts/github_api.js
+++ public/javascripts/github_api.js
@@ -35,23 +35,34 @@ async function get_readme_sha(repo_name){
 }
 
 async function edit_readme(repo_name, readme_content){
-  var sha = await get_readme_sha(repo_name);
-  console.log(sha);
-  var today = new Date();
-  var jsonDataObj = {
-    sha : sha,
-    message : today.toLocaleString() + ' edit',
-    content : readme_content};
   fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',{
-    method: 'PUT',
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
-      'Authorization': 'token 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'},
-    body: JSON.stringify(jsonDataObj)
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'}
   })
   .then(response=>response.json())
-  .then(data=>console.log(data))
+  .then(response=>{
+    console.log(response.sha);
+    return response.sha;
+    var sha = response.sha;
+    console.log(sha);
+    var today = new Date();
+    var jsonDataObj = {
+      sha : sha,
+      message : today.toLocaleString() + ' edit',
+      content : readme_content};
+    fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
+        'Authorization': 'token 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'},
+      body: JSON.stringify(jsonDataObj)
+    })
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+  })
 }
 
 function delete_repo(repo_name){
