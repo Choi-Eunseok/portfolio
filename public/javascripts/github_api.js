ac8562b07e7334d9d5c492968cd0df9d1c3f77b5
Choi-Eunseok <ted12333@gmail.com>
2021-10-02 14:56:13 +0900

    list_project

--- public/javascripts/github_api.js
+++ public/javascripts/github_api.js
@@ -68,12 +68,20 @@ function delete_repo(repo_name){
 }
 
 function list_project(text_place){
-  fetch('http://choieunseok.dothome.co.kr/list_project.php',{
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'}
-  })
-  .then(response=>response.json())
-  .then(data=>text_place.innerText(data))
+  // fetch('http://choieunseok.dothome.co.kr/list_project.php',{
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'}
+  // })
+  // .then(response=>response.json())
+  // .then(data=>text_place.innerText(data))
+
+  var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            text_place.innerText(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", "http://choieunseok.dothome.co.kr/list_project.php", true); // true for asynchronous
+    xmlHttp.send(null);
 }
