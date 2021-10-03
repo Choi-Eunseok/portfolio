fad2e92ed4b62375e1c73086b12592057da98648
Choi-Eunseok <ted12333@gmail.com>
2021-10-03 14:43:20 +0900

    db

--- public/javascripts/github_api.js
+++ public/javascripts/github_api.js
@@ -1,97 +1,117 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-east-04.cleardb.com',
-  user     : 'b7734fb5a35b0e',
-  password : 'dce9aa9e',
-  database : 'heroku_7eba0b4eba8973c'
-});
-
-connection.connect();
-
 function create_repo(repo_name){
-  var jsonDataObj = {
-    name : repo_name,
-    auto_init : true};
-  fetch('https://api.github.com/user/repos',{
+  // var jsonDataObj = {
+  //   name : repo_name,
+  //   auto_init : true};
+  // fetch('https://api.github.com/user/repos',{
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
+  //     'Authorization': 'token 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'},
+  //   body: JSON.stringify(jsonDataObj)
+  // })
+  // .then(response=>response.json())
+  // .then(data=>console.log(repo_name+" created"))
+
+  fetch('https://choi-es.herokuapp.com/dbedit/list/add',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
-      'Authorization': 'token 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'},
-    body: JSON.stringify(jsonDataObj)
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'},
+    body: {
+      'name' : repo_name,
+      'readme' : ''
+    }
   })
   .then(response=>response.json())
-  .then(data=>console.log(repo_name+" created"))
+  .then(data=>console.log(repo_name+" saved"))
 }
 
 function get_readme(repo_name){
-  fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',{
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'}
-  })
-  .then(response=>response.json())
-  .then(response=>{
-    var content = response.content;
-    console.log(atob(content));
-  })
-}
-
-function edit_readme(repo_name, readme_content){
-  fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',{
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'}
-  })
-  .then(response=>response.json())
-  .then(response=>{
-    var sha = response.sha;
-    var today = new Date();
-    var jsonDataObj = {
-      sha : sha,
-      message : today.toLocaleString() + ' edit',
-      content : btoa(readme_content)};
-    fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',{
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
-        'Authorization': 'token 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'},
-      body: JSON.stringify(jsonDataObj)
-    })
-    .then(response=>response.json())
-    .then(data=>console.log(repo_name+" edited"))
-  })
-}
-
-function delete_repo(repo_name){
-  fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name,{
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
-      'Authorization': 'token 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'}
-  })
-  console.log(repo_name+" deleted");
-}
-
-function list_project(text_place){
-  // fetch('http://choieunseok.dothome.co.kr/list_project.php',{
+  // fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',{
   //   method: 'GET',
   //   headers: {
   //     'Content-Type': 'application/json',
   //     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'}
   // })
   // .then(response=>response.json())
-  // .then(data=>text_place.innerText(data))
+  // .then(response=>{
+  //   var content = response.content;
+  //   console.log(atob(content));
+  // })
+  fetch('https://choi-es.herokuapp.com/dbedit/list/'+repo_name,{
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'}
+  })
+  .then(response=>response.json())
+  .then(response=>{
+    var content = response.readme;
+    console.log(content);
+  })
+}
 
-  var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            text_place.innerText(xmlHttp.responseText);
+function edit_readme(repo_name, readme_content){
+  // fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',{
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'}
+  // })
+  // .then(response=>response.json())
+  // .then(response=>{
+  //   var sha = response.sha;
+  //   var today = new Date();
+  //   var jsonDataObj = {
+  //     sha : sha,
+  //     message : today.toLocaleString() + ' edit',
+  //     content : btoa(readme_content)};
+  //   fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name+'/contents/README.md',{
+  //     method: 'PUT',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
+  //       'Authorization': 'token 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'},
+  //     body: JSON.stringify(jsonDataObj)
+  //   })
+  //   .then(response=>response.json())
+  //   .then(data=>console.log(repo_name+" edited"))
+  // })
+
+  fetch('https://choi-es.herokuapp.com/dbedit/list/edit',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'},
+    body: {
+      'name' : repo_name,
+      'readme' : ''
     }
-    xmlHttp.open("GET", "http://choieunseok.dothome.co.kr/list_project.php", true); // true for asynchronous
-    xmlHttp.send(null);
+  })
+  .then(response=>response.json())
+  .then(data=>console.log(repo_name+" saved"))
+}
+
+function delete_repo(repo_name){
+  // fetch('https://api.github.com/repos/Choi-Eunseok/'+repo_name,{
+  //   method: 'DELETE',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
+  //     'Authorization': 'token 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'}
+  // })
+  // console.log(repo_name+" deleted");
+
+  fetch('https://choi-es.herokuapp.com/dbedit/list/delete',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36'},
+    body: {
+      'name' : repo_name
+    }
+  })
+  .then(response=>response.json())
+  .then(data=>console.log(repo_name+" deleted"))
 }
